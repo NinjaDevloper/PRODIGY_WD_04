f145dccb960697d623b54bb10c1da917b6bed514
NinjaDevloper <155881587+NinjaDevloper@users.noreply.github.com>
2024-01-26 11:13:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function opens() {
+    document.getElementById('sidebar').style.width = "40% ";
+    document.getElementById('sidebar').style.height = "340px";
+}
+function closes() {
+    document.getElementById('sidebar').style.width = "0px"
+}
